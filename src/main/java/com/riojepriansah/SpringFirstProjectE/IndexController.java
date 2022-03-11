451661dee9b755769a49fa111007c64d6fd735ca
riojapri <80973244+riojapri@users.noreply.github.com>
2022-03-11 14:41:30 +0700

    Add files via upload

---
+++ src/main/java/com/riojepriansah/SpringFirstProjectE/IndexController.java
@@ -0,0 +1,15 @@
+package com.riojepriansah.SpringFirstProjectE;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class IndexController {
+    @GetMapping("/hello")
+    public String index(@RequestParam String nama) {
+        return "Selamat belajar spring boot! " + nama;
+    }
+}
---
+++ src/main/java/com/riojepriansah/SpringFirstProjectE/SpringFirstProjectEApplication.java
@@ -0,0 +1,13 @@
+package com.riojepriansah.SpringFirstProjectE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstProjectEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstProjectEApplication.class, args);
+	}
+
+}
